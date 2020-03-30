80474563b30fa38a1711dc3cc1eff4768d776196
Natán Candia <candianatan@fceqyn.unam.edu.ar>
2020-03-30 17:38:22 -0300

    Initial commit

---
+++ DiskPinger/bin/Debug/netcoreapp3.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+//using System.Windows.Forms;
+
+namespace DiskPinger
+{
+    class ReadFromFile
+    {
+        static void Main()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            int pings = 0;
+
+            while (true)
+            {
+                Ping();
+                pings++;
+                Console.WriteLine("Ping number " + pings);
+                Thread.Sleep(30000);
+                stopwatch.Stop();
+                //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                //Console.WriteLine(DateTime.Now.ToLongTimeString());
+                
+                stopwatch = Stopwatch.StartNew();
+            }
+        }
+        static void Ping()
+        {
+            // Example #1
+            // Read the file as one string.
+
+            
+            //MessageBox.Show("Hello, world.");
+
+            try
+            {
+                string text = System.IO.File.ReadAllText(@"D:\Ping.txt");
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("{0} Exception caught.", e.Message);
+
+                throw;
+            }
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+        }
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DiskPinger
+ Utility to ping a desired disk or resource to make a like "keep-alive" conection to it.
